87c2a93da8b882891f9af5c7fda5e3b907c06896
david-fernando <davidfernandodamata21@gmail.com>
2022-02-13 00:40:07 -0300

    Add Open JW Library

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+import time
 from classes.ZoomMeeting import ZoomMeeting
+from classes.JWLibrary import JWLibrary
 
 zoom_meeting = ZoomMeeting()
+jw_library = JWLibrary()
 
 zoom_meeting.enter_meeting()
-
+time.sleep(10)
+jw_library.open_jw_library()
